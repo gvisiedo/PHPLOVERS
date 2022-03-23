390328b2fe814dc7951d2ba496af246ce090dec0
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2022-03-23 20:39:02 +0100

    Creado el framework

---
+++ Producto2/views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1>Pagina Home</h1>
+    <p>Lorem ipsum</p>
+</div>
---
+++ Producto2/views/layout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div class="container">
+        <a href="/" class="navbar-brand h1">FW</a>
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <?php echo "home"; ?>
+    </div>
+</div>
+
+</body>
+</html>
